2223d095aabd76f2a8078fc2f89444b9e353be92
whiteshadow <whiteshadow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-10-26 11:34:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/one-click-plugin-updater/trunk@23297 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- oneclick-plugin-updater.php
+++ oneclick-plugin-updater.php
@@ -3,7 +3,7 @@
 Plugin Name: One Click Plugin Updater
 Plugin URI: http://w-shadow.com/blog/2007/10/19/one-click-plugin-updater/
 Description: Adds an "update automatically" link to plugin update notifications.
-Version: 1.0.2
+Version: 1.0.3
 Author: Janis Elsts
 Author URI: http://w-shadow.com/blog/
 */
@@ -107,19 +107,44 @@ class ws_oneclick_pup {
 	}
 	
 	function extractPlugin($zipfile) {
+		$target_dir=ABSPATH.'wp-content/plugins/';
 	    $archive = new PclZip($zipfile);
 	    $rez = false;
 	    if (function_exists('gzopen')) {
-	        if ($archive->extract(PCLZIP_OPT_PATH, ABSPATH.'wp-content/plugins/', PCLZIP_OPT_REPLACE_NEWER) != 0) {
+	        if ($archive->extract(PCLZIP_OPT_PATH, $target_dir, PCLZIP_OPT_REPLACE_NEWER) != 0) {
 		        $rez=true;
 	        };
         }
         if ((!$rez) && function_exists('exec')) {
-			exec("unzip -d ".ABSPATH."wp-content/plugins/ $zipfile");
-			$rez = true;
+			exec("unzip -d $target_dir $zipfile", $ignored, $return_val);
+			$rez = $return_val == 0;
 	    }
         return $rez;
     }
+    
+    function is__writable($path)
+	{
+	    //will work in despite of Windows ACLs bug
+	    //NOTE: use a trailing slash for folders!!!
+	    //see http://bugs.php.net/bug.php?id=27609
+	    //see http://bugs.php.net/bug.php?id=30931
+	
+	    if ($path{strlen($path) - 1} == '/') // recursively return a temporary file path
+	
+	        return is__writable($path . uniqid(mt_rand()) . '.tmp');
+	    else
+	        if (is_dir($path))
+	            return is__writable($path . '/' . uniqid(mt_rand()) . '.tmp');
+	    // check tmp file for read/write capabilities
+	    $rm = file_exists($path);
+	    $f = @fopen($path, 'a');
+	    if ($f === false)
+	        return false;
+	    fclose($f);
+	    if (!$rm)
+	        unlink($path);
+	    return true;
+	}
     
 }//class ends here
 
