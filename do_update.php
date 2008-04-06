90df53a8551683e9a503a8cf088b20ffd80b4d81
whiteshadow <whiteshadow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-06 13:38:38 +0000

    Lots of new features and bugs!
    git-svn-id: https://plugins.svn.wordpress.org/one-click-plugin-updater/trunk@38654 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- do_update.php
+++ do_update.php
@@ -1,35 +1,63 @@
 <?php
 /*
-	Update the plugin identified by $_GET['plugin_file']
+	Update the plugin(s)
 */
 	require_once("../../../wp-config.php");
+	require_once(ABSPATH . "/wp-admin/admin.php");
 	
 	if(!current_user_can('edit_plugins')) {
-			die('Oops, sorry, you are not authorized to fiddle with plugins!');
+		wp_die('Oops, sorry, you are not authorized to fiddle with plugins!');
+	}
+
+	/**
+	 * Get the main parameters
+	 */
+	$nonce = empty($_GET['_wpnonce'])?'':$_GET['_wpnonce'];
+	$action=empty($_GET['action'])?'update_plugin':$_GET['action'];
+	
+	/**
+	 * Handle reactivation
+	 */
+	if ($action == 'reactivate_all'){
+		$to_activate = get_option('plugins_to_reactivate');
+		if ( !is_array($to_activate) || (count($to_activate)<1) ){
+			//Nothing left to do!
+			wp_redirect(get_option('siteurl').'/wp-admin/plugins.php?activate=true');
+			die();
+		}
+		
+		//Redirect to this URL if a plugin crashes on activation
+		$continue_url = get_option('siteurl').'/wp-content/plugins/'.$ws_pup->myfolder.
+		 				'/do_update.php?action=reactivate_all';
+		
+		//Activate every plugin, more or less safely
+		wp_redirect($continue_url);
+		$remaining = $to_activate;
+		foreach($to_activate as $key => $plugin_file){
+			unset($remaining[$key]);
+			update_option('plugins_to_reactivate', $remaining);
+			activate_plugin($plugin_file);
+		}
+		wp_redirect(get_option('siteurl').'/wp-admin/plugins.php?activate=true');
+		die();
 	}
 	
-	//check if it's possible to write to plugins directory
+	/**
+	 * Handle plugin upgrades
+	 */
+	 
+	//check if it's possible to write to plugin directory
 	$plugin_dir=ABSPATH.'wp-content/plugins/';
 	$ws_pup->dprint("Checking to see if $plugin_dir is writable.");
 	
 	if (!$ws_pup->is__writable($plugin_dir)){
-		?>
-		<b>Error : the directory <?php echo $plugin_dir; ?> is not writable by Wordpress.</b><br/>
-		You may need to assign permissions 666, 755 or even 777 to your "plugins" directory
+		wp_die("The directory $plugin_dir; is not writable by Wordpress.<br/>
+		You may need to assign permissions 666, 755 or even 777 to your \"plugins\" directory
 		(depending on your server configuration). For more information on what file permissions are and
 		how to change them read 
-		<a href='http://www.interspire.com/content/articles/12/1/FTP-and-Understanding-File-Permissions'>Understanding file permisssions</a>.
-		<?php
-		die();
+		<a href='http://www.interspire.com/content/articles/12/1/FTP-and-Understanding-File-Permissions'>Understanding file permisssions</a>.","Plugin Folder is Not Writable");
 	} else {
 		$ws_pup->dprint('Okay.');
-	}
-	
-	$ws_pup->dprint('Loading PclZip...');
-	if (!class_exists('PclZip'))
-	{
-	    require_once ('pclzip.lib.php');
-	    $ws_pup->dprint('Okay.');
 	}
 	
 	if ($ws_pup->debug) {
@@ -40,90 +68,115 @@
 	@set_time_limit(0);
 	@ignore_user_abort(true);
 
-	/*
-	echo '<pre>';
-	print_r($_GET);
-	//*/
+	$plugin_file=empty($_GET['plugin_file'])?'':$_GET['plugin_file'];
+	$plugin_url=empty($_GET['plugin_url'])?'':$_GET['plugin_url'];
+	$download_url = empty($_GET['download_url'])?'':$_GET['download_url'];
 	
-	$plugin_file=$_GET['plugin_file'];
-	$plugin_url=$_GET['plugin_url'];
-	$ws_pup->dprint("Will update plugin '$plugin_file'. Plugin website : '$plugin_url'");
-	
-	$ws_pup->dprint("Accessing the above page to find new version's download URL.");
-	//Download the page at Wordpress.org Extend
-	$plugin_page=$ws_pup->download_page($plugin_url);
-	if (!$plugin_page) die("Error: Couldn't load URL '$plugin_url' !<br/>".
-		"You need either the CURL library installed or allow_url_fopen set in php.ini for this to work.");
-	$ws_pup->dprint("Page loaded, ".strlen($plugin_page)." bytes.");
+	$upgrades = array();
+	if ($action == 'upgrade_all'){
+		$update = get_option('update_plugins');
+		if (is_array($update->response)){
+			foreach($update->response as $file => $info){
+				if (!empty($info->package))
+					$upgrades[$file] = $info->package;
+			}
+		}
 		
-	//Get the .zip download link from the page
-	if (!preg_match('/<a\s+href=[\'"](http:\/\/[\w\.\-]*wordpress\.org[^><\'"]*\/[^><\'"]+\.zip)[\'"][^><]?>Download/i', $plugin_page, $matches)){
-		die("Error : Download link not found on $plugin_url.");
-	};
-	$download_url=$matches[1];
-	$ws_pup->dprint("Found plugin download URL : '$download_url'");
-	
-	//Deactivate the plugin (if active)
-	if ( get_option('active_plugins') ) $current_plugins = get_option('active_plugins');
-	if (!empty($current_plugins) && in_array($plugin_file, $current_plugins)) {
-		$ws_pup->dprint("The plugin that needs to be upgraded is active. Deactivating.");
-		$was_active = true;
-		$ws_pup->deActivatePlugin($plugin_file);
+		$nonce_action = $action;
 	} else {
-		$ws_pup->dprint("The plugin that needs to be upgraded is not active. Good.");
-		$was_active = false;
-	};
-	
-	//Download the new version (a ZIP file)
-	$ws_pup->dprint("Downloading new version...");
-	$zipdata = $ws_pup->download_page($download_url, 600);
-	if(!$zipdata) die("Error : couldn't download the new version from '$download_url'!<br/>".
-		"You need either the CURL library installed or allow_url_fopen set in php.ini for this to work.");
-	$ws_pup->dprint("Okay, ".strlen($zipdata)." bytes downloaded.");	
-	
-	//Save to a temporary location
-	$zipfile = tempnam("/tmp", "PLG");
-	$ws_pup->dprint("Will save the new version archive (zip) to a temporary file '$zipfile'.");
-	$handle = @fopen($zipfile, "w");
-	if(!$handle) {
-		$ws_pup->dprint("Warning: couldn't create a temporary file at '$zipfile'.");
-		//try to use the plugin's folder instead
-		$zipfile=tempnam(dirname(__FILE__), "PLG");
-		$ws_pup->dprint("Using alternate temporary file '$zipfile'.");
-		$handle = fopen($zipfile, "w");
-	}
-	if(!$handle) die("Error : couldn't create a temporary file '$zipfile'.");
-	
-	fwrite($handle, $zipdata);
-	fclose($handle);
-	unset($zipdata);
-	
-	$ws_pup->dprint("About to extract the new version.");
-	//Extract plugin files to the 'plugins' folder
-	$ws_pup->extractPlugin($zipfile) 
-		or die("Error : couldn't unzip the new version of the plugin.<br/>
-		Your server may not have ziplib installed <b>and</b> the unzip command doesn't work either.");
-	
-	$ws_pup->dprint("Plugin extracted, deleting temporary file '$zipfile'");
-	//Delete the temporary file
-	@unlink($zipfile);
-		
-	//Get activation URL for the plugin if necessary
-	if ($was_active) {
-		$ws_pup->dprint("Upgraded plugin was active. It will be reactivated by redirecting to the appropriate URL.");
-		$redirect = get_option('siteurl')."/wp-admin/".wp_nonce_url("plugins.php?action=activate&plugin=$plugin_file", 'activate-plugin_' . $plugin_file);
-		$redirect = preg_replace('~&#([0-9]+);~e', 'chr("\\1")', $redirect);
-	} else {
-		$ws_pup->dprint("Upgraded plugin was not active. Will redirect back to plugin list.");
-		$redirect = get_option('siteurl').'/wp-admin/plugins.php';
+		$upgrades[$plugin_file] = $download_url;
+		$nonce_action = 'update_plugin-'.$plugin_file;
 	}
 	
-	//Go back to the plugin tab or activation URL
-	//die("\nGo to $redirect");
-	$ws_pup->dprint("Shoould redirect to $redirect");
-	$ws_pup->dprint("(Debug version - redirection will not happen. Script execution finished.)");
-	if (!$ws_pup->debug) {
-		header("Location: $redirect");
-	};
+	/**
+	 * Nonce verification (should do this earlier?)
+	 */
+	if (!wp_verify_nonce($nonce, $nonce_action)){
+		$ws_pup->dprint("Nonce verification failed.", 3);
+		wp_die("I can't upgrade the plugin(s) because the link you used request doesn't appear to be legitimate.", 
+			"Nonce verification failed");
+	} else {
+		$ws_pup->dprint("Nonce verification passed.", 0);
+	}
+	$ws_pup->dprint("About to upgrade ".count($upgrades)." plugins.",1);
+	
+	$errors = array();
+	$to_activate = array();
+	/**
+	 * Perform all requested upgrades
+	 */
+	foreach($upgrades as $plugin_file => $download_url){
+		$ws_pup->dprint("Upgrading '$plugin_file', download URL is '$download_url'.",1);
+		
+		//Deactivate the plugin (if active)
+		if (in_array($plugin_file, get_option('active_plugins'))){
+			$ws_pup->dprint("The plugin that needs to be upgraded is active. Deactivating.",1);
+			$was_active = true;
+			$ws_pup->deActivatePlugin($plugin_file);
+		} else {
+			$ws_pup->dprint("The plugin that needs to be upgraded is not active. Good.");
+			$was_active = false;
+		}
+		
+		//Download and install
+		$plugin_info = $ws_pup->do_install($download_url,'', 'plugin');
+		if (is_wp_error($plugin_info)){
+			$errors[$plugin_file] = $plugin_info;
+		} else {
+			if (!empty($plugin_info['plugin_file'])) $plugin_file = $plugin_info['plugin_file'];  
+			
+			//Store the plugin for activation
+			if ($was_active) {
+				$ws_pup->dprint("Upgraded plugin was active. It will be reactivated.",1);
+				$to_activate[] = $plugin_file;
+			}
+		}
+	}
+	
+	$ws_pup->dprint("Main loop finished.");
+	/**
+	 * Display errors, if any.
+	 */
+	if (count($errors)>0){
+		$message = "Errors occured during upgrade. <br/>";
+		foreach ($errors as $plugin_file => $error){
+			$message .= "<strong>While upgrading $plugin_file : </strong><br/>";
+			$message = "<p>".implode("\n<br />",$error->get_error_messages())."</p>";
+		}
+		$message .= "<p><strong>The full installation log is below : </strong></p>";
+		$message .= $ws_pup->format_debug_log();
+		
+		wp_die($message, "Installer Error");
+		
+	} else {
+		/**
+		 * Redirect back to the plugin tab (sorry, no nice messages here!)
+		 */
+		if (count($to_activate)>0){
+			//Move on to reactivation
+			if (count($to_activate)==1){
+				//We can reactivate a single plugin right away.
+				$redirect = get_option('siteurl')."/wp-admin/" 
+					.wp_nonce_url("plugins.php?action=activate&plugin=$plugin_file", 
+					'activate-plugin_' . $plugin_file);
+			} else {
+				//Or handle multiple plugins - more complex.
+				update_option('plugins_to_reactivate', $to_activate);
+				$redirect=get_option('siteurl').'/wp-content/plugins/'.$ws_pup->myfolder.
+		 			'/do_update.php?action=reactivate_all';
+		 	}
+			
+		} else {
+			$redirect = get_option('siteurl')."/wp-admin/plugins.php";
+		}
+
+		$ws_pup->dprint("Should redirect to $redirect");
+		if (!$ws_pup->debug) {
+			header("Location: $redirect");
+		} else {
+			$ws_pup->dprint("(Debug version = redirection will not happen. Script execution finished.)");
+		};
+	}
+	
 	die();
 ?>
