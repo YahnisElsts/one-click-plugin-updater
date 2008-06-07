aa1801b45ef68292a53b6bd9844959b2b7b72881
whiteshadow <whiteshadow@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-06-07 20:45:58 +0000

    Fixed two silly bugs. * Fixed jQuery not properly added to the plugins/themes pages. * Removed A forgotten debug output statement.
    git-svn-id: https://plugins.svn.wordpress.org/one-click-plugin-updater/tags/2.2.1@49614 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- oneclick-plugin-updater.php
+++ oneclick-plugin-updater.php
@@ -3,7 +3,7 @@
 Plugin Name: One Click Plugin Updater
 Plugin URI: http://w-shadow.com/blog/2007/10/19/one-click-plugin-updater/
 Description: Upgrade plugins with a single click, install new plugins or themes from an URL or by uploading a file, see which plugins have update notifications enabled, control how often WordPress checks for updates, and more. 
-Version: 2.2
+Version: 2.2.1
 Author: Janis Elsts
 Author URI: http://w-shadow.com/blog/
 */
